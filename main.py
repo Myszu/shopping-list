4df34651343eabadcd5a878628fd9b515d4fcce5
Marcin <nowacki0508@gmail.com>
2026-08-31 20:27:29 +0200

    Fixed no cfg file error.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import Session
 from modules import config as cfg
 
 # LOGGING FORMATTER
-logging.basicConfig(format=f'%(asctime)s | %(levelname)s - %(message)s', datefmt='%d.%m.%Y %H:%M:%S', level=logging.INFO, filename='./logs/server.log', force=True)
+# logging.basicConfig(format=f'%(asctime)s | %(levelname)s - %(message)s', datefmt='%d.%m.%Y %H:%M:%S', level=logging.INFO, filename='./logs/server.log', force=True)
 
 
 @asynccontextmanager
--- modules/config.py
+++ modules/config.py
@@ -1,8 +1,22 @@
-import socket 
-from configparser import ConfigParser, ExtendedInterpolation
+import socket
+from configparser import ConfigParser, ExtendedInterpolation, NoSectionError
 cfg = ConfigParser(interpolation=ExtendedInterpolation(), allow_no_value=True)
+
 cfg.read("./modules/config.cfg")
 
-DEBUGGING = cfg.getboolean("Main", "debugging")
+try:
+    DEBUGGING = cfg.getboolean("Main", "debugging")
+except NoSectionError:
+    print("No config file found. Creating one.")
+    debug = input("Do you want this to be a protyping session? (y/N)")
+    debug = True if debug in ('y', 'Y') else False
+    with open("./modules/config.cfg", "w", encoding="utf-8") as file:
+        template = f"""[Main]
+debugging = {debug}
+"""
+        file.write(template)
+    cfg.read("./modules/config.cfg")
+    DEBUGGING = cfg.getboolean("Main", "debugging")
+    
 DEBUGGING_SOCKET = socket.gethostname()
 DEBUGGING_PORT = 8080
