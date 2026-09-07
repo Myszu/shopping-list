52b235db357861fa4137907b60b64d02cdf812c1
nicol-st <staszklo@o2.pl>
2026-09-07 17:38:56 +0200

    added initializer file

---
+++ modules/initializer.py
@@ -0,0 +1,20 @@
+from configparser import ConfigParser, ExtendedInterpolation, NoSectionError
+cfg = ConfigParser(interpolation=ExtendedInterpolation(), allow_no_value=True)
+
+def config_file():
+    cfg.read("./modules/config.cfg")
+
+    try:
+        DEBUGGING = cfg.getboolean("Main", "debugging")
+    except NoSectionError:
+        print("No config file found. Creating one.")
+        debug = input("Do you want this to be a protyping session? (y/N)")
+        debug = True if debug in ('y', 'Y') else False
+        with open("./modules/config.cfg", "w", encoding="utf-8") as file:
+            template = f"""[Main]
+    debugging = {debug}
+    """
+            file.write(template)
+        cfg.read("./modules/config.cfg")
+        DEBUGGING = cfg.getboolean("Main", "debugging")
+    
