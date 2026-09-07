52be0bfd2621b0dd57152e91f1800c3a44576811
Marcin <nowacki0508@gmail.com>
2026-09-07 19:34:49 +0200

    Added main endpoint/

--- main.py
+++ main.py
@@ -29,8 +29,54 @@ app: FastAPI = FastAPI(
     redoc_url=None if not cfg.DEBUGGING else "/redoc"
 )
 
+# TEMPLATES AND MOUNTS
+templates: Jinja2Templates = Jinja2Templates(directory='templates')
+app.mount('/css', StaticFiles(directory='static/css'), name='css')
+app.mount('/js', StaticFiles(directory='static/js'), name='js')
+
+# ROUTERS
+app.include_router(account.router, prefix='/acc', tags=['account', 'sign in', 'log in', 'manage'])
+
+# MAIN ENDPOINT GET METHOD
+@app.get('/', response_class=HTMLResponse)
+async def main_get(
+    request: Request,
+):
+    
+    context = {
+        "name": "Shopping List"
+    }
+    
+    return templates.TemplateResponse(request, 'index.html', context)
+
+# ERROR 404 HANDLER
+@app.exception_handler(400)
+async def custom_400_handler(
+    request: Request,
+    detail
+):
+    context = {
+        "name": "Shopping List - Error 400",
+        "code": detail.status_code,
+        "message": detail.detail
+    }
+    return templates.TemplateResponse(request, 'errors/400.html', context)
+
+# UNKNOWN ERROR HANDLER
+@app.exception_handler(HTTPException)
+async def custom_error_handler (
+    request: Request,
+    detail
+):
+    context = {
+        "name": "Shopping List - Unknown Error",
+        "code": detail.status_code,
+        "message": detail.detail
+    }
+    return templates.TemplateResponse(request, 'errors/unknown.html', context)
+
 if __name__ == "__main__": 
     if cfg.DEBUGGING:
-        uvicorn.run(app='main:app', host=cfg.DEBUGGING_SOCKET, port=cfg.DEBUGGING_PORT, reload=True)
+        uvicorn.run(app='main:app', host=cfg.DEBUGGING_SOCKET, port=cfg.DEBUGGING_PORT, reload=True, reload_excludes=["test.py"])
     else: # PROD MODE  
         uvicorn.run(app='main:app', host='0.0.0.0', port=cfg.PORT, reload=True)
